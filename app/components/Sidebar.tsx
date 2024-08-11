084e2e91c26818d7891e37799491f9396c39be6e
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-11 16:06:55 -0500

    added a sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,323 @@
+import React from "react";
+import Image from 'next/image'
+import logo from "../assets/redAlertLogo.png"
+function Sidebar() {
+  return (
+    <div>
+      <aside className="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
+        <a href="/">
+        <Image
+      src={logo}
+      width={30}
+      height={30}
+      alt="Picture of the author"
+    />
+        </a>
+
+        <div className="flex flex-col justify-between flex-1 mt-6">
+          <nav className="-mx-3 space-y-6 ">
+            <div className="space-y-3 ">
+              <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
+                Disaster preparedness
+              </label>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="/info"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-file-info"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                  <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                  <path d="M11 14h1v4h1" />
+                  <path d="M12 11h.01" />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">
+                  Creating a Plan
+                </span>
+              </a>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="/makeakit"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-first-aid-kit"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M8 8v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
+                  <path d="M4 8m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+                  <path d="M10 14h4" />
+                  <path d="M12 12v4" />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">
+                  Creating a Safety Kit
+                </span>
+              </a>
+            </div>
+
+            <div className="space-y-3 ">
+              <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
+                Types of Disasters
+              </label>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="/winterweather"
+              >
+                <svg
+                  className="h-8 w-8 text-gray-500"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  {" "}
+                  <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                  <path d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72" />{" "}
+                  <path
+                    d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                    transform="rotate(60 12 12)"
+                  />{" "}
+                  <path
+                    d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                    transform="rotate(120 12 12)"
+                  />{" "}
+                  <path
+                    d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                    transform="rotate(180 12 12)"
+                  />{" "}
+                  <path
+                    d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                    transform="rotate(240 12 12)"
+                  />{" "}
+                  <path
+                    d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                    transform="rotate(300 12 12)"
+                  />{" "}
+                </svg>
+                <span className="mx-2 text-sm font-medium">Winter Weather</span>
+              </a>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="/severeweather"
+              >
+                <svg
+                  className="h-8 w-8 text-gray-500"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  {" "}
+                  <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                  <path d="M7 18a4.6 4.4 0 0 1 0 -9h0a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7h-1" />{" "}
+                  <polyline points="13 14 11 18 14 18 12 22" />
+                </svg>
+                <span className="mx-2 text-sm font-medium">Severe Weather</span>
+              </a>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="flooding"
+              >
+                <svg
+                  className="h-8 w-8 text-gray-500"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  {" "}
+                  <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                  <path d="M7 18a4.6 4.4 0 0 1 0 -9h0a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" />{" "}
+                  <path d="M11 13v2m0 3v2m4 -5v2m0 3v2" />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">Flooding</span>
+              </a>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="#"
+              >
+                <svg
+                  className="h-8 w-8 text-gray-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                  />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">Wildfire</span>
+              </a>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="#"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-tornado"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M21 4l-18 0" />
+                  <path d="M13 16l-6 0" />
+                  <path d="M11 20l4 0" />
+                  <path d="M6 8l14 0" />
+                  <path d="M4 12l12 0" />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">Tornado</span>
+              </a>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="#"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-building"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M3 21l18 0" />
+                  <path d="M9 8l1 0" />
+                  <path d="M9 12l1 0" />
+                  <path d="M9 16l1 0" />
+                  <path d="M14 8l1 0" />
+                  <path d="M14 12l1 0" />
+                  <path d="M14 16l1 0" />
+                  <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16" />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">Earthquake</span>
+              </a>
+            </div>
+
+            <div className="space-y-3 ">
+              <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
+                Helpful Links and Resources
+              </label>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="https://www.usa.gov/"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-flag"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 5a5 5 0 0 1 7 0a5 5 0 0 0 7 0v9a5 5 0 0 1 -7 0a5 5 0 0 0 -7 0v-9z" />
+                  <path d="M5 21v-7" />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">US Government</span>
+              </a>
+
+              <a
+                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                href="https://www.redcross.org/"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-medical-cross-filled"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path
+                    d="M11 2l-.15 .005a2 2 0 0 0 -1.85 1.995v2.803l-2.428 -1.401a2 2 0 0 0 -2.732 .732l-1 1.732l-.073 .138a2 2 0 0 0 .805 2.594l2.427 1.402l-2.427 1.402a2 2 0 0 0 -.732 2.732l1 1.732l.083 .132a2 2 0 0 0 2.649 .6l2.428 -1.402v2.804a2 2 0 0 0 2 2h2l.15 -.005a2 2 0 0 0 1.85 -1.995v-2.804l2.428 1.403a2 2 0 0 0 2.732 -.732l1 -1.732l.073 -.138a2 2 0 0 0 -.805 -2.594l-2.428 -1.403l2.428 -1.402a2 2 0 0 0 .732 -2.732l-1 -1.732l-.083 -.132a2 2 0 0 0 -2.649 -.6l-2.428 1.4v-2.802a2 2 0 0 0 -2 -2h-2z"
+                    stroke-width="0"
+                    fill="currentColor"
+                  />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">Red Cross</span>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </aside>
+    </div>
+  );
+}
+
+export default Sidebar;

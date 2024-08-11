08568f0f5e4128658223e34f9950947fac6adcdd
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-11 17:08:18 -0500

    Add files via upload

---
+++ app/contact/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Image from 'next/image'
+
+export default function page() {
+  return (
+    <div className='w-screen h-screen bg-red-700 overflow-x-hidden'>
+        <Navbar />
+        <section class="bg-red-700">
+            <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">Contact Us</h2>
+                <p class="mb-8 lg:mb-16 font-light text-center text-white sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+                <form action="#" class="space-y-8">
+                    <div>
+                        <label for="email" class="block mb-2 text-md font-medium text-white">Your email</label>
+                        <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-red-700 text-sm rounded-lg block w-full p-2.5 " placeholder="name@flowbite.com" required/>
+                    </div>
+                    <div>
+                        <label for="subject" class="block mb-2 text-md font-medium text-white">Subject</label>
+                        <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm" placeholder="Let us know how we can help you" required/>
+                    </div>
+                    <div class="sm:col-span-2">
+                        <label for="message" class="block mb-2 text-md font-medium text-white">Your message</label>
+                        <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300" placeholder="Leave a comment..."></textarea>
+                    </div>
+                    <button type="submit" class="py-3 px-5 text-md font-medium text-center text-white rounded-lg bg-primary-700 border-2 border-white sm:w-fit hover:bg-white hover:text-red-700 duration-300 ">Send message</button>
+                </form>
+            </div>
+        </section>
+      </div>
+  )
+}
